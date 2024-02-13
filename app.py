cb82d2006e35c18f25a2bf259f87316ba305d0a6
Miraj Yafi <mbyafi7@outlook.com>
2024-02-12 21:18:15 -0500

    Added ability to switch pages

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/about_us')
+def about_us():
+    return render_template("about_us.html")
+
+@app.route('/recommendations')
+def recommendations():
+    return render_template("recommendations.html")
+
+@app.route('/planner')
+def planner():
+    return render_template("planner.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
