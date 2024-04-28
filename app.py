31cd34b8424180425df1b4df68f064879e98aa8c
Larry <119964953+LWcoder1@users.noreply.github.com>
2024-04-28 01:39:19 -0400

    Create unpolished version of the save plan page, functionality, and button

--- app.py
+++ app.py
@@ -3,18 +3,29 @@ import os
 from flask import Flask, render_template, url_for, request, redirect, session
 from apiTest import travelPlan, parseObjectToString
 from locationAPI import returnCoordinates
+from parseData import ParseData
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 from datetime import datetime
-
-
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 db = SQLAlchemy(app)
+allData = ParseData()
+
+
+def itineraryObjectCreator(breakfastList, lunchList, dinnerList, attractionList):
+    itineraryObject = {
+        "breakfastList": breakfastList,
+        "lunchList": lunchList,
+        "dinnerList": dinnerList,
+        "attractionList": attractionList,
+    }
+
+    return itineraryObject
 
 
 class Account(db.Model):
@@ -27,16 +38,15 @@ class Account(db.Model):
         return f'<Account {self.username}>'
 
 
-
-
-
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/map')
 def map():
     return render_template("map.html")
+
 
 @app.route('/about_us')
 def about_us():
@@ -62,9 +72,15 @@ def planner():
         days = int(request.form['totalDays'])
         travelPlans = travelPlan(ll, radius, days)
         travelPlans.dataPopulate()
+        saveObject = itineraryObjectCreator(travelPlans.breakfastList, travelPlans.lunchList, travelPlans.dinnerList,
+                                            travelPlans.attractionList)
+        session['saveObject'] = saveObject
         totalDays = []
+
         for day in range(days):
             totalDays.append(day)
+
+
         return render_template("itinerary.html",
                                duration=totalDays,
                                breakfastList=travelPlans.breakfastList,
@@ -82,7 +98,7 @@ def signUp():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        savedData = "{adsasdsadsadasd}"
+        savedData = "@"
 
         newAccount = Account(username=username, password=generate_password_hash(password), savedData=savedData)
 
@@ -90,7 +106,7 @@ def signUp():
             db.session.add(newAccount)
             db.session.commit()
 
-            return redirect("/sign-up")
+            return redirect("/login")
 
         except:
             return "There was an error with this operation"
@@ -107,6 +123,15 @@ def login():
         user = Account.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
+            currUser = Account.query.get(user.id)
+            savedData = currUser.savedData
+            savedDataList = savedData.split("@")
+            if len(savedDataList) > 1:
+                for items in range(1, len(savedDataList)):
+                    if savedDataList[items] != "":
+                        allData.addItinerary(savedDataList[items])
+            print(len(allData.dataList))
+
             return render_template(
                 "welcome.html",
                 username=username
@@ -115,6 +140,51 @@ def login():
             return "Invalid username or password"
 
     return render_template("registration.html")
+
+
+@app.route('/save', methods=['GET', 'POST'])
+def save():
+    if request.method == 'POST':
+        my_var = session.get('saveObject')
+        try:
+            user_id = session['user_id']
+            currUser = Account.query.get(user_id)
+            currUser.savedData += f"{my_var}@"
+            db.session.commit()
+            # Make it so this is my_var instead
+            allData.addItinerary(f"{my_var}")
+
+        except KeyError:
+
+            return "You need to be logged in"
+
+    return render_template("planner.html")
+    # check to make sure they are logged in. And add this to the third column saved data
+
+
+# session['user_id'] = user.id play around with this is: 100% how you determine if a player is logged in
+@app.route('/savedPlans')
+def savedPlans():
+
+    if len(allData.dataList) >= 1:
+        totalPlans = []
+        print(allData.dataList)
+        for lists in range(len(allData.dataList)):
+            totalDays = []
+            for days in range(len(allData.dataList[lists]["breakfastList"])):
+                totalDays.append(days)
+            totalPlans.append(totalDays)
+
+
+        return render_template("savedPlans.html",
+                               allPlans=totalPlans,
+                               parseString=parseObjectToString,
+                               totalList=allData.dataList
+                               )
+    else:
+        return render_template("savedPlans.html")
+
+        # look into the append thing we got at top or we have to implement a way wit js for the entire thing to loop
 
 
 
